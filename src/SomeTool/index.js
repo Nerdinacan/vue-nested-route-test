a713512bd307e234d86fcba9d1a267a6c065f8fa
Mason <nerdinacan@gmail.com>
2018-09-13 08:38:03 -0700

    First commit

---
+++ src/SomeTool/index.js
@@ -0,0 +1,5 @@
+export default {
+    install(Vue, { router }) {
+        console.log("Installing Some Tool with router", router);
+    }
+}
--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import SomeTool from './SomeTool'
+
+
+// Try to configure a module and have it add on routes
+// to the main router as part of its initialization
+// Vue.use(SomeTool(router));
+Vue.use(SomeTool, { router });
+
 
 Vue.config.productionTip = false
 
